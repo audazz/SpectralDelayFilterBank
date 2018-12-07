5b3cf7a4ea10073c2d752caaa8cecf1eed7cf14d
Chris <chris@Chriss-iMac.local>
2018-12-07 18:48:23 +0100

    Updated to JUCE 5.4.1
    Updated to JUCE 5.4.1

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -14,15 +14,18 @@
 
 #include "AppConfig.h"
 
-#include <juce_core/juce_core.h>
-#include <juce_events/juce_events.h>
-#include <juce_data_structures/juce_data_structures.h>
-#include <juce_gui_basics/juce_gui_basics.h>
-#include <juce_gui_extra/juce_gui_extra.h>
 #include <juce_audio_basics/juce_audio_basics.h>
+#include <juce_audio_devices/juce_audio_devices.h>
+#include <juce_audio_formats/juce_audio_formats.h>
 #include <juce_audio_plugin_client/juce_audio_plugin_client.h>
 #include <juce_audio_processors/juce_audio_processors.h>
+#include <juce_audio_utils/juce_audio_utils.h>
+#include <juce_core/juce_core.h>
+#include <juce_data_structures/juce_data_structures.h>
+#include <juce_events/juce_events.h>
 #include <juce_graphics/juce_graphics.h>
+#include <juce_gui_basics/juce_gui_basics.h>
+#include <juce_gui_extra/juce_gui_extra.h>
 
 
 #if ! DONT_SET_USING_JUCE_NAMESPACE
@@ -35,6 +38,7 @@
 namespace ProjectInfo
 {
     const char* const  projectName    = "SpectralDelayPlugin";
+    const char* const  companyName    = "";
     const char* const  versionString  = "1.0.0";
     const int          versionNumber  = 0x10000;
 }
